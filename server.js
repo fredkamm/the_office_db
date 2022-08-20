39d4454ac9bf4d36ba9c22c888188f1cf36f9972
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-20 12:53:40 -0400

    mysql is connected to the server.js and console.table is working so far

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// Import and require mysql2
 const mysql = require('mysql2');
+const inquirer = require('inquirer')
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -13,11 +13,24 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL username,
     user: 'root',
-    // MySQL password
     password: 'password',
-    database: 'courses_db'
+    database: 'employee_db'
   },
-  console.log(`Connected to the courses_db database.`)
+  console.log(`Connected to the employee_db database.`)
 );
+
+// Query database
+db.query('SELECT * FROM employee', function (err, results) {
+    console.table(results);
+  });
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
