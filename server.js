4e272decaa10fabf066a17399bbe6af17716b4c0
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-20 20:05:42 -0400

    user prompt has been added, and can view specific tables when selected. next is to add to the tables

--- server.js
+++ server.js
@@ -20,17 +20,90 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
-// Query database
-db.query('SELECT * FROM employee', function (err, results) {
-    console.table(results);
-  });
+// THE MAIN PROMPT
+const userPrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'What would you like to do?',
+            choices: [
+                'View all employees',
+                'Add employee',
+                'Update employee role',
+                'View all roles',
+                'Add role',
+                'View all departments',
+                'Add department',
+                'Quit'
+            ]
+        }
+    ])
+    .then((results) => {
+        const {choices} = results;
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+        if(choices === 'View all employees') {
+            viewEmployees();
+        }
+
+        if(choices === 'Add employee') {
+            console.log((choices));
+        }
+        
+        if(choices === 'Update employee role') {
+            console.log((choices));
+        }
+
+        if(choices === 'View all roles') {
+            viewRoles();
+        }
+
+        if(choices === 'Add role') {
+            console.log((choices));
+        }
+
+        if(choices === 'View all departments') {
+            viewDepartment();
+        }
+
+        if(choices === 'Add department') {
+            console.log((choices));
+        }
+
+        if(choices === 'Quit') {
+            console.log((choices));
+        }
+
+    })
+}
+
+// function to view the employees table
+const viewEmployees = () => {
+    // Query database
+    db.query('SELECT * FROM employee', function (err, results) {
+        console.table(results);
+        userPrompt();
+      });
+}
+
+// function to view the roles table
+const viewRoles = () => {
+    // Query database
+    db.query('SELECT * FROM roles', function (err, results) {
+        console.table(results);
+        userPrompt();
+      });
+}
+
+// function to view the roles table
+const viewDepartment = () => {
+    // Query database
+    db.query('SELECT * FROM department', function (err, results) {
+        console.table(results);
+        userPrompt();
+      });
+}
+
+userPrompt();
+
   
